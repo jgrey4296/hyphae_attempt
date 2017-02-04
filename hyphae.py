7f3eab9c1d034e823a06ac26a20e35b5ae03fc6b
John Grey <jgrey@ucsc.edu>
2017-02-04 09:23:26 +0000

    variable fine tuning

--- hyphae.py
+++ hyphae.py
@@ -36,11 +36,14 @@ Y = pow(2,N)
 #Start points points
 CENTRE = np.array([0.5,0.5])
 START_NODES = np.array([[0.4,0.4],[0.6,0.6]])
+#START_NODES = np.array([[0.5,0.5]])
 #Where to output the renderings:
 OUTPUT_DIR = "output"
 OUTPUT_FILE = "hyphae"
 #Render options:
-DRAW_NODES = True
+DRAW_NODES = False
+DRAW_LINES = False
+DRAW_PATHS = True
 ANIMATE = True
 SIZE_DIFF = 0.003
 LINE_PROPORTION_DISTORTION = 0.8
@@ -50,24 +53,25 @@ NODE_COLOUR = [1,1,1,NODE_OPACITY]
 node_colour = lambda : np.concatenate((random(3),[NODE_OPACITY]))
 MAIN_COLOUR = node_colour()
 DRAW_STEP = 1
+LINE_WIDTH = 0.005
 #Bounds of the quadtree:
 bounds = [0,0,1,1]
 #Neighbourhood distance size:
-HYPHAE_CIRC = 0.45
-NEIGHBOUR_DELTA = 1.6
+HYPHAE_CIRC = 0.5
+NEIGHBOUR_DELTA = 1.4
 #Growth rules:
-WIGGLE_CHANCE = 0.4
-WIGGLE_AMNT = 0.4
-WIGGLE_VARIANCE = 0.4
+WIGGLE_CHANCE = 0.7
+WIGGLE_AMNT = 0.7
+WIGGLE_VARIANCE = 0.8
 SPLIT_CHANCE = 0.5
 SPLIT_ANGLE = math.pi * 0.4
-SPLIT_ANGLE_VARIANCE = 0.4
+SPLIT_ANGLE_VARIANCE = 0.8
 NODE_START_SIZE = 0.007
-NODE_SIZE_DECAY = 0.00002
+NODE_SIZE_DECAY = 0 #0.00002
 MIN_NODE_SIZE = 0.0008
-MAX_ATTEMPTS_PER_NODE = 4
-BRANCH_BACKTRACK_AMNT = 0.5
-MAX_FRONTIER_NODES = 100
+MAX_ATTEMPTS_PER_NODE = 6
+BRANCH_BACKTRACK_AMNT = 0.8
+MAX_FRONTIER_NODES = 20
 MAX_GROWTH_STEPS = 10000
 #####################
 #Variables:
